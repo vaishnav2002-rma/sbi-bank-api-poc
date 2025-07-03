3f89571654af292f77cbb2dab230fa7ccb03201b
vaishnav2002-rma <vaishnavm.sudhish@thezennialpro.com>
2025-07-03 16:39:55 +0530

    Implemented withdraw funds, deposit funds status : In Progress

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 import asyncio
 from models import CreateAccountRequest, FundAccountRequest, WithdrawRequest 
-from services import create_customer_account
+from services import create_customer_account, fund_customer_account, withdraw_from_customer_account
 
 app = FastAPI()
 @app.post("/create-account")
@@ -19,8 +19,20 @@ async def create_account(data: CreateAccountRequest):
 
 @app.post("/fund")
 async def fund_account(data: FundAccountRequest):
-    return True
+    try:
+        balance = await fund_customer_account(data.customer_id, data.amount)
+        return {"Message" : f"Funded account id {data.customer_id} with {data.amount} and balance {balance}"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail = str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail = str(e))
 
 @app.post("/withdraw")
 async def withdraw_from_account(data: WithdrawRequest):
-    return True
+    try:
+        balance = await withdraw_from_customer_account(data.customer_id, data.amount)
+        return {"Message" : f"Amount of Rs. {data.amount} is withdrawn for customer {data.customer_id} final balance id {balance}"}
+    except ValueError as e:
+        raise HTTPException (status_Code=400, detail = str(e))
+    except Exception as e:
+        raise HTTPException (status_Code=400, detail = str(e))
--- services.py
+++ services.py
@@ -6,3 +6,18 @@ async def create_customer_account(customer_id: str):
         return False 
     accounts[customer_id] = 0.0
     return True 
+
+async def fund_customer_account(customer_id: str, amount: float):
+    if customer_id not in accounts:
+        raise ValueError (f"Account does not exist for customer with id {customer_id}")
+    accounts[customer_id] += amount 
+    return accounts[customer_id]
+
+async def withdraw_from_customer_account(customer_id: str, amount: float):
+    if customer_id not in accounts:
+        raise ValueError (f"Account does not exist for customer with id {customer_id}")
+    if accounts[customer_id] < amount:
+        raise ValueError (f"Funds not available. Current balance id {accounts[customer_id]}")
+    accounts[customer_id] -= amount 
+    return accounts[customer_id]
+
